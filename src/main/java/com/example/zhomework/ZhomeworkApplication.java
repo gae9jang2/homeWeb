e01b1937e630485dafd98d82da9e4aeff7afcf3a
gae9jang2 <gae9jang2s@naver.com>
2024-10-28 16:09:43 +0900

    프로젝트 등록

---
+++ src/main/java/com/example/zhomework/ZhomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.zhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhomeworkApplication.class, args);
+	}
+
+}
